2e39c70daeea1377a302723d98bba1d984d222cb
Bart <57799908+crypt0rr@users.noreply.github.com>
2023-04-21 13:52:15 +0200

    Update Shodan favicon | Tools OSINT (#441)

--- content/tools/osint/shodan/files/favicon.py
+++ content/tools/osint/shodan/files/favicon.py
@@ -4,7 +4,7 @@ import requests
 from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning) 
-response = requests.get('https://50.220.52.222/favicon.ico', verify=False)
+response = requests.get('https://<URL>/favicon.ico', verify=False)
 favicon = codecs.encode(response.content,"base64")
 hash = mmh3.hash(favicon)
 print(hash)
